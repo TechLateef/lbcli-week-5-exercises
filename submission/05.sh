07e0e26857b9980822b01afe83d04799d7957559
techlateef <techlateef@gmail.com>
2026-04-15 17:21:45 +0100

    fix: 05.sh to create csv script

--- submission/05.sh
+++ submission/05.sh
@@ -6,12 +6,10 @@ hex=$(printf '%08x\n' $integer | sed 's/^\(00\)*//')
 # echo $hex
 
 # no need to convert to little endian its just one byte
-timePush="01$hex"
+timePush="02$hex"
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
-publicKeyHash=$(echo "02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277" | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160 | awk '{print $NF}')
-# echo $publicKeyHash
 
 pkhPush="14$publicKeyHash"
-script="${timePush}b17576a9${pkhPush}88ac"
+script="${timePush}00b27521${publicKey}ac"
 echo $script
 # bitcoin-cli -regtest decodescript $script
