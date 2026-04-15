cb039039e005f22b6a5be7661657b8ab39a739bf
techlateef <techlateef@gmail.com>
2026-04-15 18:09:39 +0100

    fix: 05.sh

--- submission/05.sh
+++ submission/05.sh
@@ -6,10 +6,9 @@ hex=$(printf '%08x\n' $integer | sed 's/^\(00\)*//')
 # echo $hex
 
 # no need to convert to little endian its just one byte
-timePush="02$hex"
-publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
-
-pkhPush="14$publicKeyHash"
-script="${timePush}00b27521${publicKey}ac"
+timePush="029600"
+pubkey="02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277"
+pubkeyhash=$(echo $pubkey | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160 -binary | xxd -p -c 20)
+script="${timePush}b27576a914${pubkeyhash}88ac"
 echo $script
 # bitcoin-cli -regtest decodescript $script
